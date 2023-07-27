9b3140a3ae46d38b1a8ccc32620e2baae06c7013
Zimzozaur <106471045+Zimzozaur@users.noreply.github.com>
2023-07-27 09:25:31 +0200

    Added 1656

---
+++ Stack/1656_Design_an_Ordered_Stream.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/design-an-ordered-stream/
+class OrderedStream:
+
+    def __init__(self, n: int):
+
+    def insert(self, idKey: int, value: str) -> List[str]:
+
+# Your OrderedStream object will be instantiated and called as such:
+# obj = OrderedStream(n)
+# param_1 = obj.insert(idKey,value)
