69950344dd974a987c8ed9393b8170da356f8cfc
Zimzozaur <106471045+Zimzozaur@users.noreply.github.com>
2023-07-28 11:45:31 +0200

    Added 1816_Truncate_Sentence.py to arrays

---
+++ Arrays_&_Hashing/1816_Truncate_Sentence.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/truncate-sentence/
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        """
+
+        :param s:
+        :param k:
+        :return:
+        """
