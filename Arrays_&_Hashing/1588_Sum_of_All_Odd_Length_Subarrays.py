8348aeadf238df7c2ba7444875e7db2d4e689c9b
Zimzozaur <106471045+Zimzozaur@users.noreply.github.com>
2023-07-29 09:47:55 +0200

    Added 1588 to Arrays

---
+++ Arrays_&_Hashing/1588_Sum_of_All_Odd_Length_Subarrays.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
+class Solution:
+    def sumOddLengthSubarrays(self, arr: list[int]) -> int:
+        """
+
+        :param arr:
+        :return:
+        """
