6bc6c62099798b39f841e63856bf9e0970c9c9e8
Zimzozaur <106471045+Zimzozaur@users.noreply.github.com>
2023-07-29 09:21:38 +0200

    Solved 1816

--- Arrays_&_Hashing/1816_Truncate_Sentence.py
+++ Arrays_&_Hashing/1816_Truncate_Sentence.py
@@ -2,8 +2,20 @@
 class Solution:
     def truncateSentence(self, s: str, k: int) -> str:
         """
-
+        Takes string of words divided by spaces.
+        Returns k words.
         :param s:
         :param k:
         :return:
+
+        Splits string
+        Return string that is the length of a k index.
         """
+
+        new_string = s.split()
+        new_word = ""
+
+        for i in range(k):
+            new_word += new_string[i] + " "  # Builds new string and adds spaces.
+
+        return new_word[:len(new_word) - 1]  # Cuts last space.
