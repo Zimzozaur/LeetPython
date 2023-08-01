8d16e1cff9a1d82702ac23319d005f3be4f6e3e5
Zimzozaur <106471045+Zimzozaur@users.noreply.github.com>
2023-08-01 18:49:54 +0200

    Solved 74

---
+++ Binary Search/74_Search_a_2D_Matrix.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/search-a-2d-matrix/
+class Solution:
+    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
+        """
+        Performs binary search on 2D Matrix
+        :param matrix:
+        :param target:
+        :return:
+        """
+        for array in matrix:
+            pointer_1 = 0
+            pointer_2 = len(array) - 1
+            pointer_3 = int((pointer_2 + pointer_1) / 2)
+
+            if array[pointer_2] == target:  # If len is 1
+                return True
+
+            while pointer_1 <= pointer_2:  # Runs until the last digit is wrong
+
+                if array[pointer_3] == target:  # When found
+                    return True
+
+                elif array[pointer_3] < target:  # When to small move pointer_1 one above
+                    pointer_1 = pointer_3 + 1
+
+                else:
+                    pointer_2 = pointer_3 - 1  # When to big move pointer_2 one below
+
+                pointer_3 = int((pointer_2 + pointer_1) / 2)  # Recalculate pointer
+
+        return False
+
+
+if __name__ == '__main__':
+    sol = Solution()
+
+    mat = [[1]]
+    target = 0
+
+    print(sol.searchMatrix(mat, target))
