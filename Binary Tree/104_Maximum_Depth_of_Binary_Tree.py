b5413a672bb224755130f1e26402146b2d80061b
Zimzozaur <106471045+Zimzozaur@users.noreply.github.com>
2023-08-03 17:01:22 +0200

    Added Alternative Solutions

--- Binary Tree/104_Maximum_Depth_of_Binary_Tree.py
+++ Binary Tree/104_Maximum_Depth_of_Binary_Tree.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/maximum-depth-of-binary-tree/
 
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -8,6 +9,7 @@
 #         self.right = right
 from TreeNode import *
 from typing import Optional
+from collections import deque
 
 
 class Solution:
@@ -19,13 +21,45 @@ class Solution:
         :return:
         """
 
-        if not root:
-            return 0
+        # if not root:
+        #     return 0
+        #
+        # # Goes one level deeper and adds +1
+        # depth_left = self.maxDepth(root.left)
+        # depth_right = self.maxDepth(root.right)
+        #
+        # return 1 + max(depth_left, depth_right)  # Picks greater depth
 
-        # Goes one level deeper and adds +1
-        depth_left = self.maxDepth(root.left)
-        depth_right = self.maxDepth(root.right)
+        # # Breath First Search
+        #
+        # if not root:
+        #     return 0
+        #
+        # level = 0
+        # deck = deque([root])
+        # while deck:
+        #
+        #     for i in range(len(deck)):
+        #         root = deck.popleft()
+        #         if root.left:
+        #             deck.append(root.left)
+        #         if root.right:
+        #             deck.append(root.right)
+        #
+        #     level += 1
+        # return level
 
-        return 1 + max(depth_left, depth_right)  # Picks greater depth
+        # Iterative Depth first search
 
+        stack = [[root, 1]]
+        res = 0
 
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                res = max(res, depth)
+                stack.append([root.left, depth + 1])
+                stack.append([root.right, depth + 1])
+
+        return res
