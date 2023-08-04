395beecd966eddcfdcae7219c9145c31094f3b7c
Zimzozaur <106471045+Zimzozaur@users.noreply.github.com>
2023-08-04 11:21:58 +0200

    Boosted Performance of Bubble Sort

--- Z_DSA_Dojo/Bubble_Sort.py
+++ Z_DSA_Dojo/Bubble_Sort.py
@@ -5,20 +5,24 @@ def bubble_sort(arr):
 
     while k > 1:
         curr = arr[0]
+        sort_ed = False
         for i in range(1, k):
             # Swap when curr is bigger
             if arr[i] < curr:
                 temp = arr[i]
                 arr[i - 1] = temp
                 arr[i] = curr
+                sort_ed = True
             # Swap curr to bigger value
             else:
                 curr = arr[i]
+        if not sort_ed:
+            break
         k -= 1
 
 
 if __name__ == '__main__':
-    data_set = [9, 1, 8, 2, 7, 3, 6, 4, 5]
+    data_set = [1, 2, 3, 4, 5, 6]
     print(data_set)
     print()
     bubble_sort(data_set)
