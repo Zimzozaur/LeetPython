9f67abb3af475639b926c8b79f835ec7bd222003
Zimzozaur <106471045+Zimzozaur@users.noreply.github.com>
2023-08-29 13:55:23 +0200

    Optimized 543

--- Binary Tree/L543_Diameter_of_Binary_Tree.py
+++ Binary Tree/L543_Diameter_of_Binary_Tree.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import Optional
 from TreeNode import *
 
@@ -9,52 +10,28 @@ class Solution:
         The length of a path between two nodes is
         represented by the number of edges between them
 
-        Check max level of depth for every node in a
-
-        Chce rekurencyjnie dla kazegej kilki sprawdzic jak daleko mozna pojsc
-        return odległosć w prawo i lewo i zwrócić maksymalną różniece
+        Recursively go to the bottom
+        add depth by one and sum of both branches
+        check is bigger then the biggest
+        chose bigger and go up
         """
+        max_diameter = 0
+        def count_diameter(root):
+            nonlocal max_diameter
+            if not root:
+                return -1
 
-        result = 0
+            left = count_diameter(root.left)
+            right = count_diameter(root.right)
+            max_diameter = max(max_diameter, left + right + 2)
 
-        def max_depth(root, depth):
+            return max(left, right) + 1
 
-            nonlocal result
-
-            # Base Case
-            if not root.left and not root.right:
-                return depth
-
-            left_depth = None
-            right_depth = None
-
-            if root.left:
-                left_depth = max_depth(root.left, depth + 1)
-            if root.right:
-                right_depth = max_depth(root.right, depth + 1)
-
-            if isinstance(left_depth, int) and isinstance(right_depth, int):
-                curr = sum((left_depth, right_depth))
-                if curr > result:
-                    result = curr
-                return max(left_depth, right_depth)
-            elif isinstance(left_depth, int):
-                if left_depth > result:
-                    result = left_depth
-                return left_depth
-            elif isinstance(right_depth, int):
-                if right_depth > result:
-                    result = right_depth
-                return right_depth
-
-        max_depth(root, 0)
-
-        return result
-
+        count_diameter(root)
+        return max_diameter
 
 if __name__ == '__main__':
      sol = Solution()
-
      groot = TreeNode(2)
      groot.left = TreeNode(1)
      groot.right = TreeNode(3)
