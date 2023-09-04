78cfc03b76f1693b976f12fbe228f4c21fd9c51c
Zimzozaur <106471045+Zimzozaur@users.noreply.github.com>
2023-09-04 16:12:39 +0200

    Solved 105

---
+++ Binary Tree/L105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.py
@@ -0,0 +1,65 @@
+# https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+import math
+from typing import Optional
+from TreeNode import TreeNode
+from tree_generator import tree_generate
+"""
+Idziemy w lewo aż pointer na pre będzie równy pointerowi na in
+Wtedy szukamy pierwszej liczby której nie było w in
+i znowu idziemy w lewo
+
+"""
+
+class Solution:
+    def buildTree(self, preorder: list[int], inorder: list[int]) -> Optional[TreeNode]:
+        if len(preorder) == 0:
+            return
+        root = TreeNode(preorder[0])
+        ino = 0
+        for index, val in enumerate(preorder):
+            if val == inorder[0]:
+                ino = index
+
+        root.left = self.buildTree(preorder[1:ino + 1], inorder[:ino])
+        root.right = self.buildTree(preorder[ino + 1:], inorder[ino + 1:])
+
+        return root
+def in_order(root):
+    arr = []
+
+    def traverse(root):
+        if root:
+            traverse(root.left)
+            arr.append(root.val)
+            traverse(root.right)
+    traverse(root)
+    return arr
+
+
+def pre_order(root):
+    arr = []
+
+    def traverse(root):
+        if root:
+            arr.append(root.val)
+            traverse(root.left)
+            traverse(root.right)
+    traverse(root)
+    return arr
+
+if __name__ == '__main__':
+    sol = Solution()
+    x = [num for num in range(1, 6)]
+    tree = tree_generate(x)
+    pre = pre_order(tree)
+    bino = in_order(tree)
+
+    print(pre)
+    print(bino)
+    z = sol.buildTree(pre, bino)
+    print()
+
+"""
+Tworzę drzewo w lewo aż liczba będzie równa ino
+wtedy pcham ino do następnej której nnie był 
+"""
