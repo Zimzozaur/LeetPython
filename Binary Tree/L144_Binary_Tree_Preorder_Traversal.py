9cdb255f9326d5f2850819af2694101ad62a7fa6
Zimzozaur <106471045+Zimzozaur@users.noreply.github.com>
2023-09-04 17:45:53 +0200

    Tested 144

--- Binary Tree/L144_Binary_Tree_Preorder_Traversal.py
+++ Binary Tree/L144_Binary_Tree_Preorder_Traversal.py
@@ -2,6 +2,7 @@
 
 from typing import Optional
 from TreeNode import *
+from tree_generator import binary_tree_generator
 
 
 class Solution:
@@ -17,3 +18,10 @@ class Solution:
         traverse(root)
 
         return arr
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    x = [i for i in range(1, 500)]
+    tree = binary_tree_generator(x)
+    print(sol.postorderTraversal(tree))
