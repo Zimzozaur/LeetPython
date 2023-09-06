6aff60201e70e99e54b5ad6a9f5bebbab43627f0
Zimzozaur <106471045+Zimzozaur@users.noreply.github.com>
2023-09-06 16:33:57 +0200

    Added method Display that print tree as and array to TreeNode class

--- Binary Tree/TreeNode.py
+++ Binary Tree/TreeNode.py
@@ -1,6 +1,20 @@
+from collections import deque
 class TreeNode:
 
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+    def display(self):
+        arr = []
+        layer = deque([self])
+        while layer:
+            for i in range(len(layer)):
+                node = layer.popleft()
+                arr.append(node.val)
+                if node.left:
+                    layer.append(node.left)
+                if node.right:
+                    layer.append(node.right)
+        print(arr)
