dfd972d3072c6fd2587b7e93bfd9d09813d2b093
Zimzozaur <106471045+Zimzozaur@users.noreply.github.com>
2023-09-07 09:57:08 +0200

    Added List Generator to ListNode.py

--- Linked_List/ListNode.py
+++ Linked_List/ListNode.py
@@ -2,3 +2,18 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
+def list_generator(arr):
+    if len(arr) == 0:
+        return None
+    elif len(arr) == 1:
+        return ListNode(arr[0])
+
+    head = ListNode(arr[0])
+    temp = head
+    for num in arr[1:]:
+        temp.next = ListNode(num)
+        temp = temp.next
+
+    return head
