851510dce9bcb3f1d1c36ab444d0787c44ae20bc
Zimzozaur <106471045+Zimzozaur@users.noreply.github.com>
2023-09-08 18:00:53 +0200

    Solved 746

---
+++ Dynamic_Programming/L746_Min_Cost_Climbing_Stairs.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/min-cost-climbing-stairs/
+class Solution:
+    def minCostClimbingStairs(self, cost: list[int]) -> int:
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i + 1], cost[i + 2])
+        return min(cost[0], cost[1])
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    x = [10, 15, 20, 25]
+    print(sol.minCostClimbingStairs(x))
