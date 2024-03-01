e02c79d7ae1ee689ec703efd049d7062be00b45c
Zimzozaur <106471045+Zimzozaur@users.noreply.github.com>
2024-03-01 17:47:38 +0100

    Made code more readable

--- Arrays/P128_Longest_Consecutive_Sequence.py
+++ Arrays/P128_Longest_Consecutive_Sequence.py
@@ -1,7 +1,7 @@
 # https://leetcode.com/problems/longest-consecutive-sequence/description/
 
 class Solution:
-    def longestConsecutive(self, nums: list[int]) -> int:
+    def longestConsecutive(self, nums: List[int]) -> int:
         """
         The key is to find number that starts a sequence
         Number that starts a sequence is n - 1 not in set
@@ -15,9 +15,9 @@ class Solution:
         longest = 0
 
         for num in nums:
-            if num - 1 not in nums_set:
-                for i in range(1, len(nums) + 1):
-                    if num + i not in nums_set:
-                        longest = max(longest, i)
-                        break
+            if (num - 1) not in nums_set:
+                i = 1
+                while (num + i) in nums_set:
+                    i += 1
+                longest = max(longest, i)
         return longest
