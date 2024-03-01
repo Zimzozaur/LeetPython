48dc98a9805aa702fc62e62d52f3381b9791df04
Zimzozaur <106471045+Zimzozaur@users.noreply.github.com>
2024-03-01 11:52:08 +0100

    Refactor code for clarity and performance optimization
    - Shortened and optimized the code
    - Removed redundant tests

--- Binary_Tree/P1609_Even_Odd_Tree.py
+++ Binary_Tree/P1609_Even_Odd_Tree.py
@@ -1,53 +1,10 @@
 # https://leetcode.com/problems/even-odd-tree/?envType=daily-question&envId=2024-02-29
 from TreeNode import TreeNode
-from typing import Optional
 from collections import deque
 
 
-def helper(asc_decs_fn, odd_even_fn, deck: deque[TreeNode]):
-    """Returns bool about is level complain with rules"""
-    for node in deck:
-        if not odd_even_fn(node.val):
-            return False
-    if not asc_decs_fn(deck):
-        return False
-    return True
-
-
-def is_ascending(deck: deque[TreeNode]):
-    coming = deck.popleft()
-    deck.append(coming)
-    for i in range(len(deck) - 1):
-        curr = coming
-        coming = deck.popleft()
-        deck.append(coming)
-        if curr.val >= coming.val:
-            return False
-    return True
-
-
-def is_descending(deck: deque[TreeNode]):
-    coming = deck.popleft()
-    deck.append(coming)
-    for i in range(len(deck) - 1):
-        curr = coming
-        coming = deck.popleft()
-        deck.append(coming)
-        if curr.val <= coming.val:
-            return False
-    return True
-
-
-def is_even(n):
-    return n % 2 == 0
-
-
-def is_odd(n):
-    return n % 2 == 1
-
-
 class Solution:
-    def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
+    def isEvenOddTree(self, root: TreeNode) -> bool:
         """
         For every even-indexed level, all nodes at the level have odd integer values in strictly increasing order (from left to right).
 
@@ -56,18 +13,17 @@ class Solution:
         curr = deque([root])
         odd = True
         while curr:
-            if odd:
-                res = helper(is_ascending, is_odd, curr)
-            else:
-                res = helper(is_descending, is_even, curr)
-            if not res:
-                return False
-            coming = deque([])
-            for r in curr:
-                if r.left:
-                    coming.append(r.left)
-                if r.right:
-                    coming.append(r.right)
+            prev = None
+            for _ in range(len(curr)):
+                node = curr.popleft()
+                if prev and not (prev.val < node.val if odd else prev.val > node.val):
+                    return False
+                if not node.val % 2 == (1 if odd else 0):
+                    return False
+                prev = node
+                if node.left:
+                    curr.append(node.left)
+                if node.right:
+                    curr.append(node.right)
             odd = not odd
-            curr = coming
         return True
--- Binary_Tree/P1609_Even_Odd_Tree_Test.py
+++ Binary_Tree/P1609_Even_Odd_Tree_Test.py
@@ -2,174 +2,6 @@
 import unittest
 from tree_generator import tree_generate
 from P1609_Even_Odd_Tree import *
-from collections import deque
-
-
-class BasicTest(unittest.TestCase):
-
-    def test_class_exist(self):
-        sol = Solution()
-        assert isinstance(sol, Solution), 'Class is not exported'
-
-    def test_is_even_on_odd(self):
-        n = 1
-        res = is_even(n)
-        self.assertEqual(False, res)
-
-    def test_is_even_on_even(self):
-        n = 2
-        res = is_even(n)
-        self.assertEqual(True, res)
-
-    def test_is_odd_on_even(self):
-        n = 2
-        res = is_odd(n)
-        self.assertEqual(False, res)
-
-    def test_is_odd_on_odd(self):
-        n = 1
-        res = is_odd(n)
-        self.assertEqual(True, res)
-
-    def test_is_ascending_asc(self):
-        deck = deque([TreeNode(1), TreeNode(2), TreeNode(3)])
-        res = is_ascending(deck)
-        self.assertEqual(True, res)
-
-    def test_is_ascending_equal(self):
-        deck = deque([TreeNode(1), TreeNode(2), TreeNode(2)])
-        res = is_ascending(deck)
-        self.assertEqual(False, res)
-
-    def test_is_ascending_desc(self):
-        deck = deque([TreeNode(3), TreeNode(2), TreeNode(1)])
-        res = is_ascending(deck)
-        self.assertEqual(False, res)
-
-    def test_is_descending_desc(self):
-        deck = deque([TreeNode(3), TreeNode(2), TreeNode(1)])
-        res = is_descending(deck)
-        self.assertEqual(True, res)
-
-    def test_is_descending_equal(self):
-        deck = deque([TreeNode(3), TreeNode(2), TreeNode(2)])
-        res = is_descending(deck)
-        self.assertEqual(False, res)
-
-    def test_is_descending_asc(self):
-        deck = deque([TreeNode(1), TreeNode(2), TreeNode(3)])
-        res = is_descending(deck)
-        self.assertEqual(False, res)
-
-
-class TestHelperFunction(unittest.TestCase):
-    """
-    Test Generator
-    asc_desc = ['ascending', 'descending']
-    odd_even = ['odd', 'even']
-    is_asc_desc = ['is_ascending', 'is_descending']
-    is_odd_even = ['is_odd', 'is_even']
-
-    d = {
-        '00': (1, 3, 5),
-        '01': (2, 4, 6),
-        '10': (5, 3, 1),
-        '11': (6, 4, 2)
-    }
-
-    for one in range(2):
-        for two in range(2):
-            for three in range(2):
-                for four in range(2):
-                    n1, n2, n3 = d[str(three) + str(four)]
-                    print(
-                    f'''
-                    def test_{asc_desc[one]}_{odd_even[two]}_input_{asc_desc[three]}_{odd_even[four]}(self):
-                        deck = deque([TreeNode({n1}), TreeNode({n2}), TreeNode({n3})])
-                        res = helper({is_asc_desc[one]}, {is_odd_even[two]}, deck)
-                        self.assertEqual({True if one == three and two == four else False}, res)
-                    ''')
-        """
-
-    def test_ascending_odd_input_ascending_odd(self):
-        deck = deque([TreeNode(1), TreeNode(3), TreeNode(5)])
-        res = helper(is_ascending, is_odd, deck)
-        self.assertEqual(True, res)
-
-    def test_ascending_odd_input_ascending_even(self):
-        deck = deque([TreeNode(2), TreeNode(4), TreeNode(6)])
-        res = helper(is_ascending, is_odd, deck)
-        self.assertEqual(False, res)
-
-    def test_ascending_odd_input_descending_odd(self):
-        deck = deque([TreeNode(5), TreeNode(3), TreeNode(1)])
-        res = helper(is_ascending, is_odd, deck)
-        self.assertEqual(False, res)
-
-    def test_ascending_odd_input_descending_even(self):
-        deck = deque([TreeNode(6), TreeNode(4), TreeNode(2)])
-        res = helper(is_ascending, is_odd, deck)
-        self.assertEqual(False, res)
-
-    def test_ascending_even_input_ascending_odd(self):
-        deck = deque([TreeNode(1), TreeNode(3), TreeNode(5)])
-        res = helper(is_ascending, is_even, deck)
-        self.assertEqual(False, res)
-
-    def test_ascending_even_input_ascending_even(self):
-        deck = deque([TreeNode(2), TreeNode(4), TreeNode(6)])
-        res = helper(is_ascending, is_even, deck)
-        self.assertEqual(True, res)
-
-    def test_ascending_even_input_descending_odd(self):
-        deck = deque([TreeNode(5), TreeNode(3), TreeNode(1)])
-        res = helper(is_ascending, is_even, deck)
-        self.assertEqual(False, res)
-
-    def test_ascending_even_input_descending_even(self):
-        deck = deque([TreeNode(6), TreeNode(4), TreeNode(2)])
-        res = helper(is_ascending, is_even, deck)
-        self.assertEqual(False, res)
-
-    def test_descending_odd_input_ascending_odd(self):
-        deck = deque([TreeNode(1), TreeNode(3), TreeNode(5)])
-        res = helper(is_descending, is_odd, deck)
-        self.assertEqual(False, res)
-
-    def test_descending_odd_input_ascending_even(self):
-        deck = deque([TreeNode(2), TreeNode(4), TreeNode(6)])
-        res = helper(is_descending, is_odd, deck)
-        self.assertEqual(False, res)
-
-    def test_descending_odd_input_descending_odd(self):
-        deck = deque([TreeNode(5), TreeNode(3), TreeNode(1)])
-        res = helper(is_descending, is_odd, deck)
-        self.assertEqual(True, res)
-
-    def test_descending_odd_input_descending_even(self):
-        deck = deque([TreeNode(6), TreeNode(4), TreeNode(2)])
-        res = helper(is_descending, is_odd, deck)
-        self.assertEqual(False, res)
-
-    def test_descending_even_input_ascending_odd(self):
-        deck = deque([TreeNode(1), TreeNode(3), TreeNode(5)])
-        res = helper(is_descending, is_even, deck)
-        self.assertEqual(False, res)
-
-    def test_descending_even_input_ascending_even(self):
-        deck = deque([TreeNode(2), TreeNode(4), TreeNode(6)])
-        res = helper(is_descending, is_even, deck)
-        self.assertEqual(False, res)
-
-    def test_descending_even_input_descending_odd(self):
-        deck = deque([TreeNode(5), TreeNode(3), TreeNode(1)])
-        res = helper(is_descending, is_even, deck)
-        self.assertEqual(False, res)
-
-    def test_descending_even_input_descending_even(self):
-        deck = deque([TreeNode(6), TreeNode(4), TreeNode(2)])
-        res = helper(is_descending, is_even, deck)
-        self.assertEqual(True, res)
 
 
 class SolutionTests(unittest.TestCase):
@@ -209,3 +41,14 @@ class SolutionTests(unittest.TestCase):
         tree = tree_generate(tree_arr)
         res = self.sol.isEvenOddTree(tree)
         self.assertEqual(True, res)
+
+
+
+
+
+
+
+
+
+
+
