09327b49112b352bfe77fc6a872f78a8d40317ad
Simon Piechutowski <szymonpiechutowski@gmail.com>
2024-03-06 09:14:05 +0100

    Added type hints to ListNode.py

--- Linked_List/ListNode.py
+++ Linked_List/ListNode.py
@@ -1,10 +1,13 @@
+from typing import Optional
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
 
-def list_generator(arr):
+def list_generator(arr: list) -> Optional[ListNode]:
     if len(arr) == 0:
         return None
     elif len(arr) == 1:
@@ -19,7 +22,7 @@ def list_generator(arr):
     return head
 
 
-def compare_ll(head1, head2):
+def compare_ll(head1: ListNode, head2: ListNode) -> bool:
     while head1 and head2:
         if head1.val == head2.val:
             head1 = head1.next
