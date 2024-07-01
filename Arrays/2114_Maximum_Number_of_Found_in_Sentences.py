b4d91f18aee66f2050274fc27ab2063addd65c86
Simon Joseph <106471045+Zimzozaur@users.noreply.github.com>
2024-07-01 17:13:47 +0200

    Update 2114_Maximum_Number_of_Found_in_Sentences.py
    Simplfied code

--- Arrays/2114_Maximum_Number_of_Found_in_Sentences.py
+++ Arrays/2114_Maximum_Number_of_Found_in_Sentences.py
@@ -12,23 +12,4 @@ class Solution:
             if greater than max length change
             else continue
         """
-
-        # return max([len(sentences.split()) for sentences in sentences]) Or using list comprehension
-
-        maxWords = 0
-        for sentence in sentences:
-            sentence = sentence.split()  # Split sentence to words
-            words = 0
-            for _ in sentence:
-                # Counts words
-                words += 1
-            if words > maxWords:
-                # Compares max to current length
-                maxWords = words
-
-        return maxWords
-
-
-if __name__ == '__main__':
-    sol = Solution()
-    print(sol.mostWordsFound(["alice and bob love leetcode","i think so too","this is great thanks very much"]))
+        return max(len(sentences.split()) for sentences in sentences)
